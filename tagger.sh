7dd1f25fb3fbf587eba027c77dc540b227c8b7d9
makyr90 <makyr90@gmail.com>
2017-12-25 01:53:20 +0200

    Rename sample folder

--- tagger.sh
+++ tagger.sh
@@ -7,17 +7,17 @@ stdbuf -oL python3 -u tagger.py --dynet-seed 123456789 \
 --predict-batch 32 \
 --batch-tokens 3000 \
 --dropout 0.33 \
---extrn sample/gr_fasttext.npy \
---extrn-voc sample/gr_fasttext.vocab \
+--extrn data/gr_fasttext.npy \
+--extrn-voc data/gr_fasttext.vocab \
 --wembedding 100 \
 --cembedding 100 \
 --lstmlayers 2 \
 --lstmdims 200 \
 --model el_model.model \
 --params el_model.paramsdr \
---outdir sample/ \
---train sample/el-ud-train.conllu \
---dev sample/el-ud-dev.conllu |  while IFS= read -r line
+--outdir data/ \
+--train data/el-ud-train.conllu \
+--dev data/el-ud-dev.conllu |  while IFS= read -r line
 do
 tee -a model_log.txt
 done
--- tagger_predict.sh
+++ tagger_predict.sh
@@ -3,6 +3,6 @@
 python3 tagger.py --predict \
 --model el_model.model \
 --params el_model.paramsdr \
---outdir sample/ \
---test sample/el-ud-test.conllu \
+--outdir data/ \
+--test data/el-ud-test.conllu \
 --output el-ud-test.conllu.pred
