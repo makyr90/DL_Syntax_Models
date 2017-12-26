cab3a91ab22ce68eacde69eb10721aa645c280ce
makyr90 <makyr90@gmail.com>
2017-12-26 19:16:29 +0200

    Remove dist/dir embs in order to create a dedicated branch for this modification

--- biaffine.py
+++ biaffine.py
@@ -17,17 +17,7 @@ class DeepBiaffineAttentionDecoder(object):
                  arc_mlp_dropout=0.33,
                  label_mlp_dropout=0.33):
 
-        #Add lookup matrix for distance embedings in the range [-15,15]. Plus two
-        #more embeddings for distances larger than +15 and -15
-        # self.dist2id = defaultdict(lambda: len(self.dist2id))
-        # for x in range(0,17):
-        #     self.dist2id[x]
-        # for x in range(-1,-17,-1):
-        #     self.dist2id[x]
-        # self.distembs = model.add_lookup_parameters((len(self.dist2id), 64),init = dy.NormalInitializer())
 
-
-        #self.W_arc_dist = model.add_parameters((64), init = dy.ConstInitializer(0))
         Saxe_initializer = Saxe.Orthogonal(gain='leaky_relu',alpha = 0.1)
         self.src_ctx_dim = src_ctx_dim
         self.label_mlp_dropout = label_mlp_dropout
@@ -49,7 +39,6 @@ class DeepBiaffineAttentionDecoder(object):
         self.U_label_1 = [model.add_parameters((n_label_mlp_units, n_label_mlp_units), init = dy.ConstInitializer(0)) for _ in range(n_labels)]
         self.u_label_2_2 = [model.add_parameters((1, n_label_mlp_units), init = dy.ConstInitializer(0)) for _ in range(n_labels)]
         self.u_label_2_1 = [model.add_parameters((n_label_mlp_units, 1), init = dy.ConstInitializer(0)) for _ in range(n_labels)]
-        #self.W_label_dist = [model.add_parameters((64),init = dy.ConstInitializer(0)) for _ in range(n_labels)]
         self.b_label = [model.add_parameters((1,),init = dy.ConstInitializer(0)) for _ in range(n_labels)]
 
 
@@ -75,12 +64,10 @@ class DeepBiaffineAttentionDecoder(object):
 
         U_arc_1 = dy.parameter(self.U_arc_1)
         u_arc_2 = dy.parameter(self.u_arc_2)
-        #W_arc_dist = dy.parameter(self.W_arc_dist)
 
         U_label_1 = [dy.parameter(x) for x in self.U_label_1]
         u_label_2_1 = [dy.parameter(x) for x in self.u_label_2_1]
         u_label_2_2 = [dy.parameter(x) for x in self.u_label_2_2]
-        #W_label_dist = [dy.parameter(x) for x in self.W_label_dist]
         b_label = [dy.parameter(x) for x in self.b_label]
 
         if predict:
@@ -103,33 +90,16 @@ class DeepBiaffineAttentionDecoder(object):
             h_label_dep  = dy.dropout_dim(h_label_dep,1,self.label_mlp_dropout)
 
 
-        #Add W^T * dist_emb[head_i,dep_j]
-        # arc_Dist = []
-        # for row in range(src_len):
-        #     for col in range(src_len):
-        #         if ((row - col) < 16 and (row - col) > -16):
-        #             dist_emb = dy.lookup_batch(self.distembs,[self.dist2id[int(row-col)]] * batch_size)
-        #         elif ((row - col)) >= 16:
-        #             dist_emb = dy.lookup_batch(self.distembs,[self.dist2id[16]] * batch_size)
-        #         else:
-        #             dist_emb = dy.lookup_batch(self.distembs,[self.dist2id[-16]] * batch_size)
-        #
-        #         arc_Dist.append(dist_emb)
-        #
-        #
-        # arc_Dist = dy.transpose(dy.concatenate_cols(arc_Dist))
-
         h_arc_head_transpose = dy.transpose(h_arc_head)
         h_label_head_transpose = dy.transpose(h_label_head)
 
-        s_arc = h_arc_head_transpose * dy.colwise_add(U_arc_1 * h_arc_dep, u_arc_2) #+ dy.reshape((arc_Dist * W_arc_dist),(src_len,src_len),batch_size = batch_size)
+        s_arc = h_arc_head_transpose * dy.colwise_add(U_arc_1 * h_arc_dep, u_arc_2)
 
         s_label = []
         for U_1, u_2_1, u_2_2, b in zip(U_label_1, u_label_2_1, u_label_2_2, b_label):
             e1 = h_label_head_transpose * U_1 * h_label_dep
             e2 = h_label_head_transpose * u_2_1 * dy.ones((1, src_len))
             e3 = dy.ones((src_len, 1)) * u_2_2 * h_label_dep
-            #e4 = dy.reshape((arc_Dist * w_l),(src_len,src_len),batch_size = batch_size)
             s_label.append(e1 + e2 + e3 +  b)
         return s_arc, s_label
 
